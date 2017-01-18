7760b14dfbaf811c96673621793f52eca6f0f554
Abhinaya <Abhinaya@Raj.local>
2017-01-18 15:29:00 +0530

    page design implemented

--- listing.js
+++ listing.js
@@ -24,13 +24,51 @@ function display(data) {
         	{
   				upCount = post.data.ups;
         	}
-          $("#reddit-content").append( '<img class="pics" src='+ post.data.thumbnail+'>');
-          $("#reddit-content").append( '<br>' + post.data.title);
-          $("#reddit-content").append( '<br> to /r/' + post.data.subreddit);
-          $("#reddit-content").append( '<br>' + post.data.num_comments + ' comments');
-          
-          $("#reddit-content").append( '<br>' + post.data.downs );
-          
+        	var currentTime = (new Date).getTime();
+        	//console.log(currentTime);
+        	
+        	var postedTime = post.data.created_utc * 1000;
+        	//console.log(postedTime);
+        	var elapsedTime = currentTime - postedTime;
+        	//console.log(elapsedTime);
+        	var et =  timeConversion(elapsedTime);
+        	function timeConversion(millisec) {
+
+		        var seconds = (millisec / 1000).toFixed(0);
+
+		        //console.log(seconds);
+
+		        var minutes = (millisec / (1000 * 60)).toFixed(0);
+
+		        var hours = (millisec / (1000 * 60 * 60)).toFixed(0);
+		        //console.log(hours);
+
+		        var days = (millisec / (86400000)).toFixed(0);
+
+		        if (seconds < 60) {
+		            return seconds + " seconds";
+		        } else if (minutes < 60) {
+		            return minutes + " minutes";
+		        } else if (hours < 24) {
+		            return hours + " hours";
+		        } else {
+		            return days + " days"
+		        }
+		    }	
+          var item = $("<div></div>");
+          var div1 = $("<div>").addClass("vote-section");
+          div1.append("<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-up-arrow'></span></button>");
+          div1.append("<div></div>").text(upCount);
+          div1.append("<button type='button' class='btn btn-default'><span class='glyphicon glyphicon-down-arrow'></span></button>");
+
+          var image = $("<a href="+ post.data.thumbnail +"></a>").addClass("thumbnail");
+          var div3 = $("<div>").addClass("post-details");
+          div3.append("<div></div>").text(post.data.title);
+          div3.append("<div>").text("to " + post.data.subreddit);
+          div3.append("<div>").text(post.data.num_comments)
+          item.append(div1,image,div3);
+          $("#reddit-content").append(item);
+  	  
           $("#reddit-content").append( '<hr>' );
         }
     )
